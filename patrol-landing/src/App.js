765f5bb28e44c0aeb420c6ed8b98c445ac039457
Nico van Wijk <nico@patrolapp.ai>
2024-04-03 16:11:24 -0400

    Adding Logo

--- patrol-landing/src/App.js
+++ patrol-landing/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import logo from './PatrolLogo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {//<img src={logo} className="App-logo" alt="logo" />
-        }
-        <h1 className="Title">
-          Patrol
-        </h1>
+        <div className="Header-div">
+          <img src={logo} className="App-logo" alt="A Saint Bernard Dog is the logo of Patrol" />
+          <h1 className="Title">
+            Patrol
+          </h1>
+        </div>
         <h3 className="Tag-line">
           The AI first compliance platform for SMB Shopify merchants
         </h3>
         <button className="Waitlist-button" aria-label="Join our Waitlist">
-          <a  
-          className="App-link"
-          href="https://forms.gle/Mh3job9QHU2HWkza8"
-          target="_blank"
-          rel="noopener noreferrer">
+          <a
+            className="App-link"
+            href="https://forms.gle/Mh3job9QHU2HWkza8"
+            target="_blank"
+            rel="noopener noreferrer">
             Join our Waitlist
           </a>
         </button>
