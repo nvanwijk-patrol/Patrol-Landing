1bf7b8c02c30588039a1b05cb6fe35256e5ef59d
Nico van Wijk <nico@patrolapp.ai>
2024-04-03 16:19:57 -0400

    Favicon Fix + TagLine Style Changes

--- patrol-landing/src/App.js
+++ patrol-landing/src/App.js
@@ -12,7 +12,7 @@ function App() {
           </h1>
         </div>
         <h3 className="Tag-line">
-          The AI first compliance platform for SMB Shopify merchants
+          The AI first compliance platform for Shopify
         </h3>
         <button className="Waitlist-button" aria-label="Join our Waitlist">
           <a
